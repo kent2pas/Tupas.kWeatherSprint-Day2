0728e24d65d96e9ca9a46d214f4e6a3ab11905b1
kent2pas <kentcejotupas@gmail.com>
2023-01-25 15:56:07 -0800

    push

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,28 @@
-function getWeather(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=92ed4d135688a5dffbc60dc180b672ea').then(response => response.json()).then(data => console.log(data.coord.lon))
-}
+// function getWeather(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=92ed4d135688a5dffbc60dc180b672ea').then(response => response.json()).then(data => console.log(data))
+// }
 
 
-getWeather()
+// getWeather()
 
 
 
 
 // 92ed4d135688a5dffbc60dc180b672ea
+
+
+let weather = {
+    apiKey: "92ed4d135688a5dffbc60dc180b672ea",
+    fetchWeather: function(city){
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    // displayWeather: function(data){
+
+ }
