7d48910b7c3614d07663317a0058b179510f8903
kent2pas <kentcejotupas@gmail.com>
2023-01-27 15:52:36 -0800

    push

--- scripts/app.js
+++ scripts/app.js
@@ -21,8 +21,35 @@ let weather = {
           this.apiKey
       )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
-    // displayWeather: function(data){
+    displayWeather: function(data){
+    const {name} = data;
+    const {icon, description} = data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    console.log(name, icon, description, temp, humidity, speed)
+    document.querySelector(".weatherinformation4").innerText = name;
+    document.querySelector(".weatherinformation3").innerText = description;
+    document.querySelector(".weatherinformation2").innerText = temp + "°";
+    document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  },
+  
+  search: function () {
+    this.fetchWeather(document.querySelector(".inputBox").value);
+  },
+};
 
- }
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".inputBox").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+ 
+
